5ae9b3bf43fc112c963e26224f7f72aa89fe539a
Pietro Marchini <pietro.marchini@mia-platform.eu>
2023-12-28 00:37:56 +0100

    feat: wip - diagnostics-channel

--- test/diagnostics-channel/connect-error.js
+++ test/diagnostics-channel/connect-error.js
@@ -1,61 +1,61 @@
 'use strict'
 
-const t = require('tap')
+const { test } = require('node:test')
+const assert = require('node:assert')
 
 let diagnosticsChannel
 
 try {
   diagnosticsChannel = require('diagnostics_channel')
 } catch {
-  t.skip('missing diagnostics_channel')
   process.exit(0)
 }
 
 const { Client } = require('../..')
 
-t.plan(16)
-
 const connectError = new Error('custom error')
 
-let _connector
-diagnosticsChannel.channel('undici:client:beforeConnect').subscribe(({ connectParams, connector }) => {
-  _connector = connector
+test('connect error', (t) => {
+  let _connector
+  diagnosticsChannel.channel('undici:client:beforeConnect').subscribe(({ connectParams, connector }) => {
+    _connector = connector
 
-  t.equal(typeof _connector, 'function')
-  t.equal(Object.keys(connectParams).length, 6)
+    assert.equal(typeof _connector, 'function')
+    assert.equal(Object.keys(connectParams).length, 6)
 
-  const { host, hostname, protocol, port, servername } = connectParams
+    const { host, hostname, protocol, port, servername } = connectParams
 
-  t.equal(host, 'localhost:1234')
-  t.equal(hostname, 'localhost')
-  t.equal(port, '1234')
-  t.equal(protocol, 'http:')
-  t.equal(servername, null)
-})
+    assert.equal(host, 'localhost:1234')
+    assert.equal(hostname, 'localhost')
+    assert.equal(port, '1234')
+    assert.equal(protocol, 'http:')
+    assert.equal(servername, null)
+  })
 
-diagnosticsChannel.channel('undici:client:connectError').subscribe(({ error, connectParams, connector }) => {
-  t.equal(Object.keys(connectParams).length, 6)
-  t.equal(_connector, connector)
+  diagnosticsChannel.channel('undici:client:connectError').subscribe(({ error, connectParams, connector }) => {
+    assert.equal(Object.keys(connectParams).length, 6)
+    assert.equal(_connector, connector)
 
-  const { host, hostname, protocol, port, servername } = connectParams
+    const { host, hostname, protocol, port, servername } = connectParams
 
-  t.equal(error, connectError)
-  t.equal(host, 'localhost:1234')
-  t.equal(hostname, 'localhost')
-  t.equal(port, '1234')
-  t.equal(protocol, 'http:')
-  t.equal(servername, null)
-})
+    assert.equal(error, connectError)
+    assert.equal(host, 'localhost:1234')
+    assert.equal(hostname, 'localhost')
+    assert.equal(port, '1234')
+    assert.equal(protocol, 'http:')
+    assert.equal(servername, null)
+  })
 
-const client = new Client('http://localhost:1234', {
-  connect: (_, cb) => { cb(connectError, null) }
-})
+  const client = new Client('http://localhost:1234', {
+    connect: (_, cb) => { cb(connectError, null) }
+  })
 
-t.teardown(client.close.bind(client))
+  t.after(() => client.close.bind(client))
 
-client.request({
-  path: '/',
-  method: 'GET'
-}, (err, data) => {
-  t.equal(err, connectError)
+  client.request({
+    path: '/',
+    method: 'GET'
+  }, (err, data) => {
+    assert.equal(err, connectError)
+  })
 })
--- test/diagnostics-channel/error.js
+++ test/diagnostics-channel/error.js
@@ -1,52 +1,54 @@
 'use strict'
 
-const t = require('tap')
+const { test } = require('node:test')
+const assert = require('node:assert')
 
 let diagnosticsChannel
 
 try {
   diagnosticsChannel = require('diagnostics_channel')
 } catch {
-  t.skip('missing diagnostics_channel')
+  // t.skip('missing diagnostics_channel') define how to signal a skipped test
   process.exit(0)
 }
 
 const { Client } = require('../..')
 const { createServer } = require('http')
 
-t.plan(3)
-
-const server = createServer((req, res) => {
-  res.destroy()
-})
-t.teardown(server.close.bind(server))
-
-const reqHeaders = {
-  foo: undefined,
-  bar: 'bar'
-}
-
-let _req
-diagnosticsChannel.channel('undici:request:create').subscribe(({ request }) => {
-  _req = request
-})
-
-diagnosticsChannel.channel('undici:request:error').subscribe(({ request, error }) => {
-  t.equal(_req, request)
-  t.equal(error.code, 'UND_ERR_SOCKET')
-})
-
-server.listen(0, () => {
-  const client = new Client(`http://localhost:${server.address().port}`, {
-    keepAliveTimeout: 300e3
+test('connect error', async (t) => {
+  const server = createServer((req, res) => {
+    res.destroy()
   })
-  t.teardown(client.close.bind(client))
+  t.after(() => server.close.bind(server))
 
-  client.request({
-    path: '/',
-    method: 'GET',
-    headers: reqHeaders
-  }, (err, data) => {
-    t.equal(err.code, 'UND_ERR_SOCKET')
+  const reqHeaders = {
+    foo: undefined,
+    bar: 'bar'
+  }
+
+  let _req
+  diagnosticsChannel.channel('undici:request:create').subscribe(({ request }) => {
+    _req = request
+  })
+
+  diagnosticsChannel.channel('undici:request:error').subscribe(({ request, error }) => {
+    assert.equal(_req, request)
+    assert.equal(error.code, 'UND_ERR_SOCKET')
+    server.close() // TODO: is this the right place to close the server?
+  })
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, {
+      keepAliveTimeout: 300e3
+    })
+    t.after(client.close.bind(client))
+
+    client.request({
+      path: '/',
+      method: 'GET',
+      headers: reqHeaders
+    }, (err, data) => {
+      assert.equal(err.code, 'UND_ERR_SOCKET')
+    })
   })
 })
